c4c29e6f4db330755ce02a2cfabe3d7418400448
PudhariSwaroopa <pudhariswaroopa.11@gmail.com>
2026-03-12 11:14:07 +0530

    Files added

---
+++ template.sh
@@ -0,0 +1,12 @@
+# create dataset folder
+mkdir -p data
+
+# create notebook folder (optional)
+mkdir -p knowledge
+
+# create files
+touch knowledge/smart-knowledge-assistant.ipynb
+touch requirements.txt
+touch .env
+
+echo "Notebook project structure created successfully"
